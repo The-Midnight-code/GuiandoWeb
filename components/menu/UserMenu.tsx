e2fbaf90338b604b2482f3f89c0713377901fd17
Ismael Lares <ismael.lardom@gmail.com>
2026-08-16 13:24:11 -0700

    fix sign out button

--- components/menu/UserMenu.tsx
+++ components/menu/UserMenu.tsx
@@ -34,7 +34,9 @@ const UserMenu = () => {
             Your Profile
           </a>
         </MenuItem>
-        <SignOutButton>
+        <SignOutButton
+          redirectUrl={process.env.NEXT_PUBLIC_CLERK_SIGN_OUT_REDIRECT_URL}
+        >
           <MenuItem key={2}>
             <a className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden">
               Sign out
